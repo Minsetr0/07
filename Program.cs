f07a25f1231d28ce417a2826640ad437de1a04ee
Minsetr0 <nesterovmaksim71@mail.ru>
2023-03-09 16:30:15 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Привет! Какое имя ты бы хотел поместить внутрь прямоугольника?");
+string name = Console.ReadLine();
+Console.WriteLine("В какие символы его поместить?");
+string symbol = Console.ReadLine();
+for (int i = 0; i < name.Length; i++)
+{
+    Console.Write("\n" + symbol);
+}
+Console.WriteLine(symbol + name + symbol);
+for (int i = 0; i < name.Length; i++)
+{
+    Console.Write("\n" + symbol);
+}
